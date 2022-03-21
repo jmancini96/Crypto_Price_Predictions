3516e3be3c1ece573144dfac36dd828c98acadcb
Your name here <brkmer@gmail.com>
2022-03-21 13:15:58 -0400

    initial React commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import About from './components/About';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+function About() {
+    return (
+    <section className="my-5" >
+        <div>
+            <h1> insert text here if (condition) {
+                
+            } needed </h1>
+
+        </div>
+        {/* <img src={coverImage} className="my-2" style={{ width: "60%" }} alt="cover" /> */}
+        <div className="my-2">
+
+        </div>
+    </section>
+    )
+
+
+
+
+}
+
+export default About;
