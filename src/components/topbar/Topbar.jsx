f09c1c0d8aa0f6a5a29229310793f1fba6a7b40f
Your name here <brkmer@gmail.com>
2022-03-24 13:37:07 -0400

    nav bar complete

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./topbar.css";
-import   AppsIcon  from '@mui/icons-material/Apps';
-
+import  AppsIcon  from '@mui/icons-material/Apps';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import Brulejpg from "../../assets/Brule.jpg"
 
 export default function Topbar(){
     return(
@@ -13,7 +15,17 @@ export default function Topbar(){
                 <div className="topRight">
                     <div className="topbarIcons">
                         <AppsIcon></AppsIcon>
+                        <span className="topIconBag">2</span>
                     </div>
+                    <div className="topbarIcons">
+                        <SettingsApplicationsIcon></SettingsApplicationsIcon>
+                        
+                    </div>
+                    <div className="topbarIcons">
+                        <AttachMoneyIcon></AttachMoneyIcon>
+                       
+                    </div>
+                    <img src="https://pyxis.nymag.com/v1/imgs/29b/bae/50c47f603f465c28cc385853c6a36169c1-29-steve-brule-check-it-out.rsquare.w700.jpg" alt="" className="topPFP" />
                 </div>
 
             </div>
