416de7c0bd0103b9c80e9f9eef50a68da715e6e7
Your name here <brkmer@gmail.com>
2022-05-31 16:18:44 -0400

    added largeWidget and Table 1

--- src/components/LargeWidget/largeWidget.jsx
+++ src/components/LargeWidget/largeWidget.jsx
@@ -3,7 +3,16 @@ import "./LargeWidget.css"
 export default function largeWidget() {
   return (
     <div className="largeWidget">
-        large
+        <h3 className="largeWidgetTitle">Latest transactions</h3>
+        <table className="largeWidgetTable">
+          <tr className="largeWidgetTr">
+            <th className="largeWidgetTh">User</th>
+            <th className="largeWidgetTh">Date</th>
+            <th className="largeWidgetTh">Amount</th>
+            <th className="largeWidgetTh">Status</th>
+          </tr>
+        
+        </table>
     </div>
   )
 }
