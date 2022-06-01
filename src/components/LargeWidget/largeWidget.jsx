2faa7764474b7d736e32ed0bba1a08f84b5c83c3
Your name here <brkmer@gmail.com>
2022-05-31 23:11:57 -0400

    Widgets finished

--- src/components/LargeWidget/largeWidget.jsx
+++ src/components/LargeWidget/largeWidget.jsx
@@ -2,14 +2,14 @@ import "./LargeWidget.css"
 
 export default function largeWidget() {
   const Button = ({type}) =>{
-    return <button className={"largeWidgetButton" + type}>({type})</button>
+    return <button className={"largeWidgetButton " + type}>({type})</button>
   }
   return (
     <div className="largeWidget">
         <h3 className="largeWidgetTitle">Latest transactions</h3>
         <table className="largeWidgetTable">
           <tr className="largeWidgetTr">
-            <th className="largeWidgetTh">User</th>
+            <th className="largeWidgetTh">Wallet</th>
             <th className="largeWidgetTh">Date</th>
             <th className="largeWidgetTh">Amount</th>
             <th className="largeWidgetTh">Status</th>
