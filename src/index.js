c131e4d500758c434f3bc350e395677f6e402c88
Your name here <brkmer@gmail.com>
2022-06-06 17:58:43 -0400

    removed yarn

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import express from 'express';
 import App from './App';
 
-App.use(express.static(__dirname));
+// App.use(express.static(__dirname));
 
-App.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+// App.get("/*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
 
 ReactDOM.render(
   <React.StrictMode>
