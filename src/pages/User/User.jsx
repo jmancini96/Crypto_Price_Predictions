f70b17981d75c5fa47e6b50a79dd705c44cd720d
Your name here <brkmer@gmail.com>
2022-06-07 17:32:05 -0400

    attempted webpack.config.js fix

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-
-export default function User() {
-  return (
-    <div>User</div>
-=======
 import "./User.css"
 
 export default function User() {
@@ -13,6 +6,5 @@ export default function User() {
         User
 
     </div>
->>>>>>> c131e4d500758c434f3bc350e395677f6e402c88
   )
 }
